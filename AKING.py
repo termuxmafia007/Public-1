52cf358bb1091ee5b378a841c5b53f60e63702d6
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-05-29 19:07:23 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,7 +1,17 @@
-import os
-print(' \033[1;32m Welcome My Tolls\033[1;37m')
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
 if __name__ == "__main__":
 	try:
 		__import__("Public").login()
 	except Exception as e:
 		exit(str(e))
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
