a673ad47c580b84c8e9f47b1b5a5708890816d1d
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-14 02:59:38 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -13,8 +13,7 @@ if bit == '64bit':
     from AKING import login
     login()
 elif bit == '32bit':
-    print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print("\x1b[1;91m Sorry ! Your Device Support Not this Tools")
     print(' Join Over Facebook Group For Any Help 😍 ')
     os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
-    print('Soon')
     exit()
