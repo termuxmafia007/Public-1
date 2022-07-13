275e3423a9b2b650d6135c3a19c08ff0a2ffe816
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-14 01:32:52 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -16,3 +16,23 @@ if __name__ == "__main__":
 		exit(str(e))
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    from AKING import AKING
+    AKING()
+elif bit == '32bit':
+    print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    from AKING32 import main
+    main()
