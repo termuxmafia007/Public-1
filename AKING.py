c241dd48ed5c9ce373b374e5c3d24c6f297c5c08
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-14 17:07:58 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,19 +1,10 @@
-import os, platform, time
-try:
-    import requests
-except:
-    os.system('pip install requests')
-    os.system('git pull')
-    import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    print(' Join Over Facebook Group For Any Help 😍 ')
+import os,time
+
+def Update():
+    print("\n\x1b[1;92m Updating :-/")
+    print(' Join Over Facebook Group')
     os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
-    from AKING import login
-    login()
-elif bit == '32bit':
-    print("\x1b[1;91m Sorry ! Your Device Support Not this Tools")
-    print(' Join Over Facebook Group For Any Help 😍 ')
-    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
-    exit()
+   # from AKING import login
+  #  login()
+    input('')
+Update():
