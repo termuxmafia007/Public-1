14046ea784a3ff26c366cd9ba099590bdf5f48c5
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-14 17:19:14 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,4 +1,6 @@
 import os,time
+def clear():
+	os.system('clear')
 def banner():
 	clear()
 	print("""
@@ -18,7 +20,7 @@ def banner():
 def Update():
     banner()
     print("\n\x1b[1;92m Updating Soon :-/")
-    print('Please Wait For Update 😊 ')
+    print(' Please Wait For Update 😊 ')
     print(' Join Over Facebook Group')
     os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
     input('')
