78c7bc33f356a4b5698fb902952cd87782839402
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-31 16:43:26 +0500

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,9 @@
+import time
+print('[•] Enjoy Random Cloning')
+def Random():
+    print('[•] Random Cloning Open Source');time.sleep(1)
+    print('[•] Random Cloning Open Source');time.sleep(1)
+    print('[•] Random Cloning Open Source');time.sleep(1)
+    print('[•] Random Cloning Open Source');time.sleep(1)
+    Random()
+Random()
