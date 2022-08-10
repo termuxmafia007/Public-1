7afd26f96698d31c5bb97ef43f1f2f76d59270ef
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-08-10 23:06:53 +0500

    Delete Random.py

--- Random.py
+++
@@ -1,9 +0,0 @@
-import time
-print('[•] Enjoy Random Cloning')
-def Random():
-    print('[•] Random Cloning Open Source');time.sleep(1)
-    print('[•] Random Cloning Open Source');time.sleep(1)
-    print('[•] Random Cloning Open Source');time.sleep(1)
-    print('[•] Random Cloning Open Source');time.sleep(1)
-    Random()
-Random()
