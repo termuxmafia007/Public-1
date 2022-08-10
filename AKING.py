05768da4c217d253c17c94031eb36780aa19a729
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-08-10 23:15:31 +0500

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,12 @@
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('[•] Join Over Facebook Group For Any Help')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import AKING
+else:
+    print('\033[1;31m[×] Sorry Device Not Support')
